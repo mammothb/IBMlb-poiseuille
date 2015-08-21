16874a708b4e2350644fd9bb6fba147edf468b3f
mammothb <mammothb+git@gmail.com>
2015-08-21 23:51:41 +0800

    translations and formatting

--- src/ibm.cpp
+++ src/ibm.cpp
@@ -10,93 +10,85 @@
 using namespace std;
 
 /**
-*	Función phi_2 para calcular función delta de Dirac con soporte de dos nodos
-*	@param double r, Distancia entre nodos (Lagrangiana - Euleriana)
-*   @return d, ponderación para distancia
-*/
+ *	Phi_2 function to calculate Dirac delta function with support of two nodes
+ *	@param double r, Distance between nodes (Lagrangian - Eulerian)
+ *  @return d, distance weighting
+ */
 double phi_2(double r)
 {
 	double phi = 0.0;
-	if ((0.0 <= fabs(r)) &&  (fabs(r)<= 1.0))
-        phi = (1.0-fabs(r));
-    if ( 1.0 <= fabs(r))
-        phi = 0.0;
-    return phi;
+	if (0.0 <= fabs(r) && fabs(r) <= 1.0) phi = 1.0 - fabs(r);
+  if (1.0 <= fabs(r)) phi = 0.0;
+  return phi;
 }
 
 /**
-*	Función phi_3 para calcular función delta de Dirac con soporte cuatro
-*	@param double r, Distancia entre nodos (Lagrangiana - Euleriana)
-*   @return double d, ponderación para distancia 700,  10100
-*/
+ *	Phi_3 function to calculate Dirac delta function with four support
+ *	@param double r, Distance between nodes (Lagrangian - Eulerian)
+ *  @return double d, distance weighting 700, 10100
+ */
 double phi_3(double r)
 {
 	double phi = 0.0;
-	if((0.0 <= fabs(r)) && ( fabs(r) <= (1./2.)))
-        phi = ((1./3.)*(1.0+sqrt(1.0-3.0*r*r)));
-    if(((1./2.) <= fabs(r))&&(fabs(r) <= (3./2.)))
-        phi = ((1./6.)*(5.0-3.0+fabs(r)-sqrt(-2.0+6.0*fabs(r)-3.0*r*r)));
-    if((3./2.) <= fabs(r))
-        phi = 0.0;
-    return phi;
+	if (0.0 <= fabs(r) && fabs(r) <= 0.5) phi = (1.0 / 3.0 * (1.0 + sqrt(1.0 - 3.0 * r * r));
+  if (0.5 <= fabs(r) && fabs(r) <= 1.5) phi = (1.0 / 6.0 * (5.0 - 3.0 + fabs(r) - sqrt(-2.0 + 6.0 * fabs(r) - 3.0 * r * r));
+  if (1.5 <= fabs(r)) phi = 0.0;
+  return phi;
 }
 
 /**
-*	Función phi_4 para calcular función delta de Dirac con soporte cuatro
-*	@param double r, Distancia entre nodos (Lagrangiana - Euleriana)
-*   @return d, ponderación para distancia
-*/
+ *	Phi_4 function to calculate Dirac delta function with four support
+ *	@param double r, Distance between nodes (Lagrangian - Eulerian)
+ *  @return d, distance weighting
+ */
 double phi_4(double r)
 {
 	double phi = 0.0;
-	if((0.0 <= fabs(r)) && (fabs(r) <= 1.0))
-        phi = ((1./8.)*(3.0-(2.0*fabs(r))+sqrt(1.0+(4.0*fabs(r))-(4.0*r*r))));
-    if((1.0 <= fabs(r)) && (fabs(r) <= 2.0))
-        phi = ((1./8.)*(5.0-(2.0*fabs(r))-sqrt(-7.0+(12.0*fabs(r))-(4.0*r*r))));
-    if(fabs(r) >= 2.0)
-        phi = 0.0;
-    return phi;
+	if (0.0 <= fabs(r) && fabs(r) <= 1.0) phi = (0.125 * (3.0 - 2.0 * fabs(r) + sqrt(1.0 + 4.0 * fabs(r) - (4.0 * r * r))));
+  if (1.0 <= fabs(r) && fabs(r) <= 2.0) phi = (0.125 * (5.0 - 2.0 * fabs(r) - sqrt(-7.0 + 12.0 * fabs(r) - (4.0 * r * r))));
+  if(fabs(r) >= 2.0) phi = 0.0;
+  return phi;
 }
 
 /**
-*	Función dirac_4 para calcular función delta de Dirac con soporte cuatro
-*	@param x, Vector distancia entre nodos (Lagrangiana - Euleriana)
-*   @return d, ponderación por distancia
-*/
+ *	Dirac_2 function to calculate Dirac delta function with four support
+ *	@param x, Vector distance between nodes (Lagrangian - Eulerian)
+ *  @return d, distance weighting
+ */
 double dirac_2(double *x)
 {
-    double d = phi_2(x[0])*phi_2(x[1])*phi_2(x[2]);
+    double d = phi_2(x[0]) * phi_2(x[1]) * phi_2(x[2]);
     return d;
 }
 
 /**
-*	Función dirac_3 para calcular función delta de Dirac con soporte cuatro
-*	@param x, Vector distancia entre nodos (Lagrangiana - Euleriana)
-*   @return d, ponderación por distancia
-*/
+ *	Dirac_3 function to calculate Dirac delta function with four support
+ *	@param x, Vector distance between nodes (Lagrangian - Eulerian)
+ *  @return d, distance weighting1
+ */
 double dirac_3(double *x)
 {
-    double d = phi_3(x[0])*phi_3(x[1])*phi_3(x[2]);
+    double d = phi_3(x[0]) * phi_3(x[1]) * phi_3(x[2]);
     return d;
 }
 
 /**
-*	Función dirac_4 para calcular función delta de Dirac con soporte cuatro
-*	@param x, Vector distancia entre nodos (Lagrangiana - Euleriana)
-*   @return d, ponderación por distancia
-*/
+ *	Dirac_4 function to calculate Dirac delta function with four support
+ *	@param x, Vector distance between nodes (Lagrangian - Eulerian)
+ *  @return d, distance weighting1
+ */
 double dirac_4(double *x)
 {
-    double d = phi_4(x[0])*phi_4(x[1])*phi_4(x[2]);
+    double d = phi_4(x[0]) * phi_4(x[1]) * phi_4(x[2]);
     return d;
 }
 
 /**
-* TODO: Implementar las funcion spread
-*/
+ * TODO: Implement spread function
+ */
 void spread(fluid fluido, mesh membrana, int x, int y, int z)
 {
-	// Recorrer todos los nodos del fluido
+	// Browse all nodes in the fluid
 	double pos[3]={0.0,0.0,0.0}, distancia[3]={0.0,0.0,0.0}, delta, df[3]={0.0,0.0,0.0}, fNodo[3]={0.0,0.0,0.0};
 	double a, A, b, B, c, C;
 	int nodos = membrana.darNumeroNodos();
@@ -140,11 +132,11 @@ void spread(fluid fluido, mesh membrana, int x, int y, int z)
 
 
 /**
-* TODO: Implementar las funcion interpolation
-*/
+ * TODO: Implement the interpolation function
+ */
 void interpolation(fluid fluido, mesh membrana, int x, int y, int z)
 {
-	//Recorrer todos los nodos de la malla
+	// Loop through all mesh nodes
 	double pos[3], distancia[3], delta, a, A, b, B, c, C, ux=0.0, uy=0.0, uz=0.0;
 	int nNodos = membrana.darNumeroNodos();
 	for(int u=0;u<nNodos;u++)
@@ -157,9 +149,9 @@ void interpolation(fluid fluido, mesh membrana, int x, int y, int z)
 		c = pos[2]-3.0;
 		C = pos[2]+3.0;
 
-		for(int i = (int) a;i<A;i++)
-			for(int j = (int) b;j<B;j++)
-				for(int k=(int) c;k<C;k++)
+		for(int i = (int) a; i < A; i++)
+			for(int j = (int) b; j < B; j++)
+				for(int k= (int) c; k < C; k++)
 				{
 					int posj = j;
 					distancia[0]=pos[0]-i;
